7e2e3df3cb69c7cdc3f3bbc96fa47430c71922fb
frontexas <forfron@gmail.com>
2017-12-03 17:38:49 -0600

    Implement throw_bowling_ball

--- bowling_game/BowlingGame.py
+++ bowling_game/BowlingGame.py
@@ -2,6 +2,8 @@ from random import randint
 from Frames import Frames
 
 class BowlingGame:
+
+
 	def __init__(self):
 		self.frames = Frames()
 		self.current_frame_index = 0
@@ -23,44 +25,43 @@ class BowlingGame:
 
 		self.frames[peek].extra_score += score
 
-		
+		# TODO pop from the queue if the particular index has been updated two times 
 
-	def throw_bowling_ball(self):
-		if self.frames[self.current_frame_index].is_first_throw:
+	def get_score_for_current_throw(self, index_for_this_throw):
+		score = 0
+		if self.frames[index_for_this_throw].is_first_throw:
 			score = randint(0, 10)
 		else: 
-			score = randint(0, 10 - self.frames[self.current_frame_index].get_first_throw_score())
-
-		# second throw already, go to next index
-		if not self.frames[self.current_frame_index].is_first_throw:
-			self.current_frame_index += 1
-
-		# strike
-		if score == 10 and self.frames[self.current_frame_index].is_first_throw:
-			self.current_frame_index += 1
-
-		self.frames[self.current_frame_index].update_score(score)
-
-		if self.frames[self.current_frame_index].get_first_throw_score() + self.frames[self.current_frame_index].get_first_throw_score() == 10: 
-			self.handle_spare(score)
-
-		if self.frames[self.current_frame_index].get_first_throw_score() == 10: 
-			self.handle_strike(score)
+			# if it's a second throw, the score range is 0 to 10 - (how many pins the player knocked down in first throw)
+			score = randint(0, 10 - self.frames[index_for_this_throw].get_first_throw_score())
+		return score
 
 
+	def throw_bowling_ball(self):
+		index_for_this_throw = self.current_frame_index
+		score = self.get_score_for_current_throw(index_for_this_throw)
 
+		self.frames[index_for_this_throw].update_score(score)
 
+		is_having_a_strike = score == 10 and self.frames[index_for_this_throw].is_first_throw
+		is_having_a_spare = not is_having_a_strike and (self.frames[index_for_this_throw].get_first_throw_score() + self.frames[index_for_this_throw].get_second_throw_score() == 10)
 		
+		# strike, go to next index
+		if is_having_a_strike:
+			self.current_frame_index += 1
+		
+		# second throw already, go to next index
+		if not self.frames[index_for_this_throw].is_first_throw:
+			self.current_frame_index += 1
 
+		self.handle_strike(score)
+		self.handle_spare(score)
 
-
-
-
-
-
-
-
-
-
-
-
+		if is_having_a_strike:
+			self.strike_queue.append(index_for_this_throw)
+		if is_having_a_spare:
+			self.spare_queue.append(index_for_this_throw)
+		
+		# mark that the player just did a first throw	
+		if self.frames[index_for_this_throw].is_first_throw:
+			self.frames[index_for_this_throw].toggle_is_first_throw()
