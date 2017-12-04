354365ebb3ea24274020b5e7c408d8756b7c9dd4
frontexas <forfron@gmail.com>
2017-12-04 14:18:30 -0600

    Create Frames model

--- bowling_game/models.py
+++ bowling_game/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Frames(models.Model):
+	first_throw = models.CharField()
+	second_throw = models.CharField()
+	total_score = models.CharField()
+
+	def __str__(self):
+		return {'first_throw': self.first_throw, 'second_throw': self.second_throw, 'total_score': self.total_score}
