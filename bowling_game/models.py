ce7da54b7c7a0f8b5c3e6b6f131ecdb6cd454629
frontexas <forfron@gmail.com>
2017-12-05 08:50:56 -0600

    Create many-to-one relationship between Frame and BowlingGame

--- bowling_game/models.py
+++ bowling_game/models.py
@@ -4,24 +4,31 @@ from django.db import models
 from random import randint
 from django.contrib import admin
 
-class Frame: 
-	is_first_throw = True
-	first_throw_score = 0
-	second_throw_score = 0
-	extra_score = 0
+class Frame(models.Model): 
+	is_first_throw = models.BooleanField(default=True)
+	first_throw_score = models.IntegerField(default=0)
+	second_throw_score = models.IntegerField(default=0)
+	extra_score = models.IntegerField(default=0)
+	bowling_game = models.ForeignKey('BowlingGame', on_delete=models.CASCADE, related_name='frames')
 
 	def update_score(self, score):
 		self._add_to_the_correct_throw(score)
-		self.total_score = self.first_throw_score + self.second_throw_score
+		self.save()
+
+	def add_extra_score(self, score):
+		self.extra_score += score
+		self.save()
 
 	def toggle_is_first_throw(self):
 		self.is_first_throw = False
+		self.save()
 
 	def _add_to_the_correct_throw(self, score):
 		if self.is_first_throw:
 			self.first_throw_score += score 
 		else:
 			self.second_throw_score += score 
+		self.save()
 
 	def get_first_throw_score(self):
 		return self.first_throw_score
@@ -44,35 +51,35 @@ class BowlingGame(models.Model):
 
 	def __init__(self):
 		models.Model.__init__(self)
-		self.frames = [Frame() for i in range(10)]
 
 	def add_new_striked_frame(self, frame_index_of_the_throw):
-		striked_frame = StrikedFrame(index = frame_index_of_the_throw, bowling_game = self)
-		striked_frame.save()
-		self.striked_frames.add(striked_frame)
+		striked_frame = StrikedFrame(index = frame_index_of_the_throw, bowling_game=self)
+		self.striked_frames.add(striked_frame, bulk=False)
+		self.save()
 
 	def handle_strike(self, score):
 		if len(self.striked_frames.all()) == 0: return 
 
 		for i, striked_frame in enumerate(self.striked_frames.all()):
 			if abs(striked_frame.index - self.throw_index_of_the_game) <= 2: 
-				self.frames[striked_frame.index].extra_score += score
+				self.frames.all()[striked_frame.index].add_extra_score(score)
 				striked_frame.update_count += 1 
+		self.save()
 
 	def handle_spare(self, score):
 		if self.spare_index == -1: return
 
-		self.frames[self.spare_index].extra_score += score
+		self.frames.all()[self.spare_index].add_extra_score(score)
 		self.spare_index = -1
 		self.save()
 
 	def get_score_for_current_throw(self, frame_index_of_the_throw):
 		score = 0
-		if self.frames[frame_index_of_the_throw].is_first_throw:
+		if self.frames.all()[frame_index_of_the_throw].is_first_throw:
 			score = randint(0, 10)
 		else: 
 			# if it's a second throw, the score range is 0 to 10 - (how many pins the player knocked down in first throw)
-			score = randint(0, 10 - self.frames[frame_index_of_the_throw].get_first_throw_score())
+			score = randint(0, 10 - self.frames.all()[frame_index_of_the_throw].get_first_throw_score())
 		return score
 
 	def update_frame_index_of_the_game_if_necessary(self, is_a_strike, frame_index_of_the_throw):
@@ -81,11 +88,13 @@ class BowlingGame(models.Model):
 			self.frame_index_of_the_game += 1
 		
 		# second throw already, go to next index
-		if not self.frames[frame_index_of_the_throw].is_first_throw:
+		if not self.frames.all()[frame_index_of_the_throw].is_first_throw:
 			self.frame_index_of_the_game += 1
+		self.save()
 
 	def update_score_for_current_frame(self, frame_index, score):
-		self.frames[frame_index].update_score(score)
+		self.frames.all()[frame_index].update_score(score)
+		self.save()
 
 	def update_spare_index(self, frame_index_of_the_throw):
 		self.spare_index = frame_index_of_the_throw
@@ -95,8 +104,8 @@ class BowlingGame(models.Model):
 		self.update_score_for_current_frame(frame_index_of_the_throw, score)
 
 		is_a_strike = score == 10
-		is_a_spare = not is_a_strike and (self.frames[frame_index_of_the_throw].get_first_throw_score() + self.frames[frame_index_of_the_throw].get_second_throw_score() == 10)
-		
+		is_a_spare = not is_a_strike and (self.frames.all()[frame_index_of_the_throw].get_first_throw_score() + self.frames.all()[frame_index_of_the_throw].get_second_throw_score() == 10)
+
 		self.update_frame_index_of_the_game_if_necessary(is_a_strike, frame_index_of_the_throw)		
 
 		self.handle_strike(score)
@@ -108,12 +117,14 @@ class BowlingGame(models.Model):
 			self.update_spare_index(frame_index_of_the_throw)
 		
 		# mark that the player just did a first throw	
-		if self.frames[frame_index_of_the_throw].is_first_throw:
-			self.frames[frame_index_of_the_throw].toggle_is_first_throw()
+		if self.frames.all()[frame_index_of_the_throw].is_first_throw:
+			self.frames.all()[frame_index_of_the_throw].toggle_is_first_throw()
+		self.save()
 
 	def throw_bowling_ball(self):
 		frame_index_of_the_throw = self.frame_index_of_the_game
 		score_of_the_throw = self.get_score_for_current_throw(frame_index_of_the_throw)
 		self.update_frames(score_of_the_throw, frame_index_of_the_throw)
 		self.throw_index_of_the_game += 1
+		self.save()
 
--- bowling_game/tests.py
+++ bowling_game/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from mock import patch
 
 from .models import BowlingGame
+from .models import Frame
 
 BOWLING_GAME_MODULE_PATH = 'bowling_game.models.BowlingGame'
 
@@ -13,27 +14,39 @@ class TestBowlingGame(TestCase):
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', return_value=5)
 	def test_to_see_if_first_throw_score_updated_accordingly_when_the_player_get_open_frame(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
-		self.assertEqual(bowling_game.frames[0].get_first_throw_score(), 5)
+		self.assertEqual(bowling_game.frames.all()[0].get_first_throw_score(), 5)
 	
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', return_value=3)
 	def test_to_see_if_second_throw_score_updated_accordingly_when_the_player_get_open_frame(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
-		self.assertEqual(bowling_game.frames[0].get_second_throw_score(), 3)
+		self.assertEqual(bowling_game.frames.all()[0].get_second_throw_score(), 3)
 
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', return_value=5)
 	def test_to_see_if_spared_frame_updated_accordingly_when_the_player_get_open_frame(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
-		self.assertEqual(bowling_game.frames[0].get_total_score(), 15)
+		self.assertEqual(bowling_game.frames.all()[0].get_total_score(), 15)
 
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', side_effect=[10, 3, 2])
 	def test_to_see_if_striked_frame_updated_accordingly_when_the_player_get_open_frame(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 
 		# throwing a strike on first frame
 		bowling_game.throw_bowling_ball()
@@ -44,12 +57,15 @@ class TestBowlingGame(TestCase):
 		# throwing a 2
 		bowling_game.throw_bowling_ball()
 
-		self.assertEqual(bowling_game.frames[0].get_total_score(), 15)
+		self.assertEqual(bowling_game.frames.all()[0].get_total_score(), 15)
 
 
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', side_effect=[3, 2])
 	def test_to_see_if_frames_index_updated_accordingly_when_the_player_get_open_frame(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		self.assertEqual(bowling_game.frame_index_of_the_game, 1)
@@ -58,25 +74,31 @@ class TestBowlingGame(TestCase):
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', side_effect=[5, 5, 3, 7, 4])
 	def test_to_see_if_multiple_spare_updates_the_frame_accordingly(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
-		self.assertEqual(bowling_game.frames[0].get_total_score(), 13)
-		self.assertEqual(bowling_game.frames[1].get_total_score(), 14)
-		self.assertEqual(bowling_game.frames[2].get_total_score(), 4)
+		self.assertEqual(bowling_game.frames.all()[0].get_total_score(), 13)
+		self.assertEqual(bowling_game.frames.all()[1].get_total_score(), 14)
+		self.assertEqual(bowling_game.frames.all()[2].get_total_score(), 4)
 
 	@patch(BOWLING_GAME_MODULE_PATH + '.get_score_for_current_throw', side_effect=[10, 10, 10, 5, 2])
 	def test_to_see_if_multiple_strike_updates_the_frame_accordingly(self, mocked_function):
 		bowling_game = BowlingGame.objects.create()
+		for i in range(10):
+			frame = Frame(bowling_game=bowling_game)
+			bowling_game.frames.add(frame, bulk=False)
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
 		bowling_game.throw_bowling_ball()
-		self.assertEqual(bowling_game.frames[0].get_total_score(), 30)
-		self.assertEqual(bowling_game.frames[1].get_total_score(), 25)
-		self.assertEqual(bowling_game.frames[2].get_total_score(), 17)
-		self.assertEqual(bowling_game.frames[3].get_total_score(), 7)
+		self.assertEqual(bowling_game.frames.all()[0].get_total_score(), 30)
+		self.assertEqual(bowling_game.frames.all()[1].get_total_score(), 25)
+		self.assertEqual(bowling_game.frames.all()[2].get_total_score(), 17)
+		self.assertEqual(bowling_game.frames.all()[3].get_total_score(), 7)
 
