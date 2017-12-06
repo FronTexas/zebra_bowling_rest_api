7cc5f636627c5f0be1997edbb3d76fe6e6232091
frontexas <forfron@gmail.com>
2017-12-06 11:42:25 -0600

    Add the necessary field to FrameSerializer

--- bowling_game/serializers.py
+++ bowling_game/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Frame
 
 class FrameSerializer(serializers.ModelSerializer):
-
 	class Meta: 
 		model = Frame
-		fields = ('first_throw_score', 'second_throw_score', 'third_throw_score')
+		fields = ('first_throw_score', 'second_throw_score', 'third_throw_score', 'total_score')
