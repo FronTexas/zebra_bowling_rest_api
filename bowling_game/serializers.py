5fd923fe3992302d37ffdaa9f4f1437c499e9282
frontexas <forfron@gmail.com>
2017-12-06 11:05:41 -0600

    Create FrameSerializer

---
+++ bowling_game/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers 
+from .models import Frame
+
+class FrameSerializer(serializers.ModelSerializer):
+
+	class Meta: 
+		model = Frame
+		fields = ('first_throw_score', 'second_throw_score', 'third_throw_score')
