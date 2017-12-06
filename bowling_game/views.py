c4619fa184f1c25566e23b3b4a0a893ad2e6bd59
frontexas <forfron@gmail.com>
2017-12-06 11:06:12 -0600

    Import FrameSerializer to views.py

--- bowling_game/views.py
+++ bowling_game/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import BowlingGame
-from .serializers import StockSerializer
+from .serializers import FrameSerializer
 
 class BowlingGameView(APIView):
 
